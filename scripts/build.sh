3015933eff582c0be791d49e97cffafa5350505e
Ignasi Fosch <natx@y10k.ws>
2021-08-26 17:29:54 +0200

    Fix build script to compile correct commands

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,5 @@ OS=${1:-$(uname -s | tr -s '[A-Z]' '[a-z')}
 ARCH=${2:-$(uname -m | sed -e 's/x86_/amd/g')}
 
 for command in $(ls cmd); do
-    GOOS=${OS} GOARCH=${ARCH} go build -o build/$command.${OS}_${ARCH} ./cmd/list
+    GOOS=${OS} GOARCH=${ARCH} go build -o build/$command.${OS}_${ARCH} ./cmd/$command
 done
